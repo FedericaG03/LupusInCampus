1c08e08ff7656e3740f927a69dce33281059b331
fedeg <fedegraz03@gmail.com>
2025-02-09 20:30:58 +0100

    Started comunication with server server for lobby

--- app/src/main/java/com/example/lupusincampus/API/LobbyAPI.java
+++ app/src/main/java/com/example/lupusincampus/API/LobbyAPI.java
@@ -1,7 +1,13 @@
 package com.example.lupusincampus.API;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.lupusincampus.Play.GestioneLogicaPartita.LobbyDatabaseHelper;
 import com.example.lupusincampus.ServerConnector;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.List;
@@ -12,27 +18,51 @@ public class LobbyAPI {
     private static final String TAG = "LobbyAPI";
 
 
-
-    public class LobbyCallback implements ServerConnector.CallbackInterface{
-
-        @Override
-        public void onSuccess(JSONObject jsonResponse) {
-            ;
-        }
-
-        public List<Objects> onSuccess(JSONObject response, int a){
-            return null;
-        }
-
-        @Override
-        public void onError(String jsonResponse) {
-
-        }
-
-        @Override
-        public void onServerError(Exception e) {
-
-        }
+    public void showLobbyRequest(Context ctx, ServerConnector.CallbackInterface callback){
+        serverConnector.makeGetRequest(ctx,"controller/lobby/active-public-lobbies", callback);
     }
+    public void doShowLoddy(Context ctx) {
+        Log.d(TAG, "doShowLoddy: Richiesta al server per le lobby");
+
+        showLobbyRequest(ctx, new ServerConnector.CallbackInterface() {
+            @Override
+            public void onSuccess(JSONObject jsonResponse) {
+                try{
+                    // Crea un'istanza di LobbyDatabaseHelper e salva i dati
+                    LobbyDatabaseHelper dbHelper = new LobbyDatabaseHelper(ctx);
+
+                    for(int i = 0; i < jsonResponse.length(); i++){
+                        JSONObject lobbies = jsonResponse.getJSONObject("lobby");
+                        int code = lobbies.getInt("code");
+                        int creatorID = lobbies.getInt("creatorID");
+                        String creationDate = lobbies.getString("creationDate");
+                        int minNumPlayer = lobbies.getInt("minNumPlayer");
+                        int numPlayer = lobbies.getInt("numPlayer");
+                        int maxNumPlayer = lobbies.getInt("maxNumPlayer");
+                        String type = lobbies.getString("type");
+                        String state = lobbies.getString("state");
+
+                        dbHelper.insertLobby(code, creatorID, creationDate, minNumPlayer, numPlayer, maxNumPlayer, type, state);
+                    }
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+            }
+
+            @Override
+            public void onError(String jsonResponse) {
+                Toast.makeText(ctx, jsonResponse, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onServerError(Exception e) {
+                Toast.makeText(ctx, "Il server non risponde , errore nel caricamento delle lobby!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
 
--- app/src/main/java/com/example/lupusincampus/Play/GestioneLogicaPartita/LobbyListActivity.java
+++ app/src/main/java/com/example/lupusincampus/Play/GestioneLogicaPartita/LobbyListActivity.java
@@ -1,5 +1,6 @@
 package com.example.lupusincampus.Play.GestioneLogicaPartita;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +8,13 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.lupusincampus.API.LobbyAPI;
 import com.example.lupusincampus.R;
-import com.example.lupusincampus.ServerConnector;
-
-import org.json.JSONObject;
 
 public class LobbyListActivity extends AppCompatActivity {
+
+    private LobbyAdapter adapter;
+    private LobbyDatabaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,66 +22,30 @@ public class LobbyListActivity extends AppCompatActivity {
         setContentView(R.layout.mostra_lobby_view);
 
         ListView listView = findViewById(R.id.my_list_view_lobby);
+        dbHelper = new LobbyDatabaseHelper(this);
 
+        // Richiede le lobby al server
+        LobbyAPI lobbyAPI = new LobbyAPI();
+        lobbyAPI.doShowLoddy(getApplicationContext());
+
+        // Recupera i dati salvati nel database
+        Cursor cursor = dbHelper.getAllLobbies();
+        adapter = new LobbyAdapter(this, cursor);
+        listView.setAdapter(adapter);
+
+        // Aggiunge un header alla lista
         LayoutInflater inflater = getLayoutInflater();
         View headerView = inflater.inflate(R.layout.lobby_list_header, listView, false);
         listView.addHeaderView(headerView);
+    }
 
-        ServerConnector serverConnector = new ServerConnector();
-        // mostra lobby attive endpoint = controller/lobby/active-public-lobbies
-        //GET
-        //MI MANDI
-
-        // Richiedi i dati dal server
-        serverConnector.fetchDataForListView(this,new ServerConnector.CallbackInterface() {
-            @Override
-            public void onSuccess(JSONObject jsonResponse) {
-
-            }
-
-            @Override
-            public void onError(String jsonResponse) {
-
-            }
-
-            @Override
-            public void onServerError(Exception e) {
-
-            }
-
-            /*@Override
-            public void onSuccess(String jsonResponse) {
-                runOnUiThread(() -> {
-                    try {
-                        JSONArray jsonArray = new JSONArray(jsonResponse);
-                        JSONObject[] lobbyData = new JSONObject[jsonArray.length()];
-
-                        // Converti la risposta JSON in un array di JSONObject
-                        for (int i = 0; i < jsonArray.length(); i++) {
-                            lobbyData[i] = jsonArray.getJSONObject(i);
-                        }
-
-                        // Crea l'adapter e imposta i dati
-                        LobbyAdapter adapter = new LobbyAdapter(LobbyListActivity.this, lobbyData);
-                        listView.setAdapter(adapter);
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        Toast.makeText(LobbyListActivity.this, "Errore nel parsing dei dati", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-            @Override
-            public void onError(Exception e) {
-                runOnUiThread(() ->
-                        Toast.makeText(LobbyListActivity.this, "Errore durante il fetch dei dati", Toast.LENGTH_SHORT).show()
-                );
-            }
-
-            @Override
-            public void onServerOffline(Exception e) {
-                Toast.makeText(LobbyListActivity.this, "Errore connessione server", Toast.LENGTH_SHORT).show();
-            }*/
-        });
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Aggiorna i dati quando l'activity torna visibile
+        Cursor cursor = dbHelper.getAllLobbies();
+        adapter = new LobbyAdapter(this, cursor);
+        ListView listView = findViewById(R.id.my_list_view_lobby);
+        listView.setAdapter(adapter);
     }
 }
